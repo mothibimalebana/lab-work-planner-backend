9d907820608c9a509ca2a7c7fc85ebe0a257ec81
mothibimalebana <mothibi.mm12@gmail.com>
2026-03-24 22:56:13 +0200

    tested Booking module

--- LWP_Backend/api/admin.py
+++ LWP_Backend/api/admin.py
@@ -4,5 +4,3 @@ from scheduler.models import Module, Booking, Slot, Student
 # Register your models here.
 admin.site.register(Module)
 admin.site.register(Booking)
-admin.site.register(Slot)
-admin.site.register(Student)
