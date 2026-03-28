a640b35eb3338336564996e908c3baedca6dde35
mothibimalebana <mothibi.mm12@gmail.com>
2026-03-28 16:13:38 +0200

    registered: Student, Shift and Schedule model to admin site

--- LWP_Backend/api/admin.py
+++ LWP_Backend/api/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from scheduler.models import Module, Booking, Slot, Student
+from scheduler.models import Module, Booking, Slot, Shift, Schedule, Student
 
 # Register your models here.
 admin.site.register(Module)
 admin.site.register(Booking)
 admin.site.register(Slot)
-admin.site.register(Student)    
+admin.site.register(Student)
+admin.site.register(Shift)
+admin.site.register(Schedule)
